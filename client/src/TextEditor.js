4d464d357f148a7de9a3539ba93560e12e5ab39b
Kritik29 <Kritik29@users.noreply.github.com>
2021-05-01 20:15:50 -0400

    socket connection

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,6 +1,7 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
+import { io } from 'socket.io-client';
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,13 +17,39 @@ const TOOLBAR_OPTIONS = [
 
 export default function TextEditor() {
 
+    const [socket, setSocket] = useState();
+    const [quill, setQuill] = useState();
+
+    useEffect(() => {
+        const s = io("http://localhost:3001", { transports: ['websocket', 'polling', 'flashsocket'] });
+        setSocket(s);
+
+        return () => {
+            s.disconnect();
+        }
+    }, []);
+
+    useEffect(() => {
+        const handler = (delta, oldDelta, source) => {
+            if (source !== 'user') return;
+            socket.emit("send-changes", delta);
+        };
+
+        quill.on('text-change', handler )
+
+        return () => {
+            quill.off('text-change', handler)
+        }
+    }, [socket, quill]);
+
     const wrapperRef = useCallback((wrapper) => {
         if (wrapper == null) return
 
         wrapper.innerHTML = '';
         const editor = document.createElement('div');
         wrapper.append(editor);
-        new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS} });
+        const q = new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS} });
+        setQuill(q);
     }, []);
 
     return ( 
